8d10dc8d7a10f96a68d0058b5be22bf029a7370b
jci <anurag.d.sharma@jci.com>
2019-12-02 18:03:36 +0530

    added one more library

--- TestStaticLibrary/ViewController.swift
+++ TestStaticLibrary/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import DemoStaticLib
+import ACryptor
 
 class ViewController: UIViewController {
+    let tokenizer = Tokenizer() // 2
+    let service = AuthenticationService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let loginToken = service.login("try", "coding");
+        let token = tokenizer.generateToken("user", "S7eo#0-2K&b") // 3
+               print("token: \(token)") // 4
+        print("login Token :\(loginToken)")
     }
 
 
