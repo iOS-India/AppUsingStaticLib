dd9256bf30c7a000e6960d24d6cba7ba30034df7
jci <anurag.d.sharma@jci.com>
2019-12-02 13:14:41 +0530

    Initial Commit

---
+++ TestStaticLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestStaticLibrary
+//
+//  Created by Anurag Sharma on 02/12/19.
+//  Copyright © 2019 Anurag Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
